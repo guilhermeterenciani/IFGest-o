a97e09211774b06408ee96b4926b0201de6e9c50
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2022-12-29 00:40:09 -0400

    👔 ♻️ Calendário do Plano de ensino

--- src/planoEnsino/content.js
+++ src/planoEnsino/content.js
@@ -1,3 +1,9 @@
+import Swal from "sweetalert2"
+
+import {
+    MODAL_CONFIRM_OPTIONS,
+    MODAL_LOADING_OPTIONS,
+} from "../utils/sweetAlert"
 import {
     getDadosPropostaTrabalho,
     getConteudoDiario,
@@ -30,12 +36,38 @@ btnGerarProposta.classList.add("btn-warning")
 divAdicionarProposta.appendChild(btnGerarProposta)
 
 btnGerarProposta.addEventListener("click", async () => {
-    gerarProposta()
+    Swal.fire({
+        ...MODAL_CONFIRM_OPTIONS,
+        title: "Gerar Calendário da Proposta",
+        text: `Este procedimento gera  o calendário da proposta do plano de ensino de forma automática, deseja continuar?`,
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            const numeroDeAulasDaProposta = await getNumeroAulasProposta()
+
+            if (numeroDeAulasDaProposta > 0) {
+                Swal.hideLoading()
+                Swal.update({
+                    title: "Não foi possível gerar o calendário da proposta",
+                    icon: "error",
+                    html: `
+                        <p>
+                            Já existe uma proposta cadastrada para este plano de ensino!  
+                        </p>
+                    `,
+                })
+            } else {
+                await gerarProposta()
+                document.location.reload()
+            }
+        }
+    })
 })
 
-// modal aguarde
-
 async function gerarProposta() {
+    Swal.update({
+        title: "Gerando Calendário da Proposta",
+        text: "aguarde...",
+    })
     const aulas = await criarListaDatasDeAula()
     if (!aulas) return false
 
@@ -50,6 +82,7 @@ async function criarListaDatasDeAula() {
     let docHTML = parser.parseFromString(response, "text/html")
     let tableConteudoDoDiario =
         docHTML.querySelectorAll("table.diario")[1] ?? null
+
     if (!tableConteudoDoDiario) {
         return Promise.resolve(false)
     }
@@ -87,8 +120,9 @@ async function criarListaDatasDeAula() {
                 ),
                 qtde: listaDataDasAulas.length,
                 sabadoLetivo:
-                    listaDataDasAulas[listaDataDasAulas.length - 1].getDay() ===
-                        6 ?? false,
+                    listaDataDasAulas[
+                        listaDataDasAulas.length - 1
+                    ]?.getDay() === 6 ?? false,
             }
         },
     )
@@ -102,11 +136,15 @@ function getNumeroAulasUC() {
 }
 
 async function getNumeroAulasProposta() {
+    Swal.fire({
+        ...MODAL_LOADING_OPTIONS,
+        title: "Consultando aulas no Plano de Ensino",
+        text: "Aguarde...",
+    })
     const dadosProposta = await getDadosPropostaTrabalho(
         idDiario,
         idPlanoEnsino,
     )
-    hideModal()
     const qtdeAulasProposta = dadosProposta.aaData
         .map((item) => {
             return item[2]
