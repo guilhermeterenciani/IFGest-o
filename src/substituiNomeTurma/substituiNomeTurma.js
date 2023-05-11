e648cfd36682e115734dca05996524207a10ca55
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-05-11 15:49:12 -0400

    [New Feature] Abreviação do nome da turma

---
+++ src/substituiNomeTurma/substituiNomeTurma.js
@@ -0,0 +1,15 @@
+//console.log("Guilherme Terenciani");
+//alert("Guilherme Terenciani");
+
+const nomeTurma = [
+    {"nomeDiario":"20231098105B","turma":"Turma 1021-B"},
+    {"nomeDiario":"20231098105A","turma":"Turma 1021-A"}
+]; 
+const table = document.querySelectorAll(".table.table-striped.table-bordered.table-condensed")
+const tdClasse = table[0].childNodes[3].querySelectorAll("td:nth-child(1)")
+for(let td of tdClasse){
+    let classe = (td.innerText).substring(0, 12);
+    if(nomeTurma.find((item)=>item.nomeDiario==classe)!=undefined){
+        td.innerHTML = nomeTurma.find((item)=>item.nomeDiario==classe).turma + td.innerHTML;
+    }
+}
